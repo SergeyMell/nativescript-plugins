dacaacb3187a076f10604038baf58597a116a84a
Sergey Mell <sergey.mell@agilie.com>
2020-10-23 02:49:15 +0300

    Added angular support for the color-wheel plugin

--- apps/demo-angular/src/plugin-demos/color-wheel.component.ts
+++ apps/demo-angular/src/plugin-demos/color-wheel.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone } from '@angular/core';
 import { DemoSharedColorWheel } from '@demo/shared';
 import {} from '@sergeymell/color-wheel';
+import { Color } from '@nativescript/core';
 
 @Component({
 	selector: 'demo-color-wheel',
@@ -8,10 +9,15 @@ import {} from '@sergeymell/color-wheel';
 })
 export class ColorWheelComponent {
 	demoShared: DemoSharedColorWheel;
+	color: Color = new Color('#0000FF');
 
 	constructor(private _ngZone: NgZone) {}
 
 	ngOnInit() {
 		this.demoShared = new DemoSharedColorWheel();
 	}
+
+  onColorSelected(args) {
+    this.color = args.object;
+  }
 }
--- apps/demo-angular/src/plugin-demos/color-wheel.module.ts
+++ apps/demo-angular/src/plugin-demos/color-wheel.module.ts
@@ -1,10 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
 import { ColorWheelComponent } from './color-wheel.component';
+import { NativeScriptColorWheelModule } from '@sergeymell/color-wheel/angular';
 
 @NgModule({
-	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: ColorWheelComponent }])],
-	declarations: [ColorWheelComponent],
-	schemas: [NO_ERRORS_SCHEMA],
+  imports: [
+    NativeScriptColorWheelModule,
+    NativeScriptCommonModule,
+    NativeScriptRouterModule.forChild([{ path: '', component: ColorWheelComponent }])],
+  declarations: [ColorWheelComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class ColorWheelModule {}
+export class ColorWheelModule {
+}
